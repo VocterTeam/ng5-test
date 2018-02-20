00d63a1a411d4eeb8daeac1cd750e8ead7b3edd0
Arthur <AmeliePoulain-92@mail.ru>
2018-02-20 21:58:27 +0200

    change output of the post request login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,24 +11,23 @@ export class AppComponent {
   public captchaResponse: string = '';
   public apiURL: string = 'https://api.amalyze.com/0.0.12';
   public reCaptchaKey: string = '6LcKNEIUAAAAAPGe3VDGzUcqXyi5iyMD-IPhiXej';
-  public httpOptions = {
-  	headers: new HttpHeaders({
-  	  'Content-Type':  'application/json',
-  	  'Authorization': 'my-auth-token'
-  	})
-	};
+ //  public httpOptions = {
+ //  	headers: new HttpHeaders({
+ //  	  'Content-Type':  'application/json',
+ //  	  'Authorization': 'my-auth-token'
+ //  	})
+	// };
 
   constructor(private http: HttpClient) { }
 
-  public captchaResolved(captchaResponse: string) {
-    
-    this.captchaResponse = captchaResponse;
-    console.log(captchaResponse);
+    public captchaResolved(captchaResponse: string) {
+      
+      this.captchaResponse = captchaResponse;
 
-    return this.http.post(`${this.apiURL}/system.user.login`, {
-    	username: 'developertest@amalyze.com',
-    	password_md5: '909027439581d5d4923cb83fedd711e8',
-    	captcha: this.captchaResponse
-    }).subscribe(res => console.log(res));
-  }
+      return this.http.post(`${this.apiURL}/system.user.login`, {
+      	username: 'developertest@amalyze.com',
+      	password_md5: '909027439581d5d4923cb83fedd711e8',
+      	captcha: this.captchaResponse
+      }).subscribe(res => console.log(res));
+    }
 }
