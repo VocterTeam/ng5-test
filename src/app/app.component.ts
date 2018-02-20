804854038f74502e16ded3eea442f66eed1b231e
Arthur <arthur.mirzoyan@vocter.com>
2018-02-20 17:10:40 +0200

    add dynamic headers http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,27 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title = 'Ng5-test';
   public captchaResponse: string = '';
+  public apiURL: string = 'https://app.swaggerhub.com/apis/AMALYZEAG/Falcon/0.0.12';
   public reCaptchaKey: string = '6LcKNEIUAAAAAPGe3VDGzUcqXyi5iyMD-IPhiXej';
+  public httpOptions = {
+  	headers: new HttpHeaders({
+  	  'Content-Type':  'application/json',
+  	  'Authorization': 'my-auth-token'
+  	})
+	};
+
+  constructor(private http: HttpClient) { }
 
   public captchaResolved(captchaResponse: string) {
     
     this.captchaResponse = `${JSON.stringify(captchaResponse)}`;
     console.log(captchaResponse);
+
+    return this.http.post(`${this.apiURL}/system.user.login/`, {
+    	username: 'developertest@amalyze.com',
+    	password: 'Iilo1ail',
+    	captcha: this.captchaResponse
+    }).subscribe(res => console.log(res));
   }
 }
  
